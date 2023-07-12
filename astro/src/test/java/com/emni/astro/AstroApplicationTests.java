c4d199aca6d413a32ed015f6643cff979864c31d
Nik-tech-cloud <62603376+Nik-tech-cloud@users.noreply.github.com>
2023-07-12 10:40:45 +0200

    Project strucure

---
+++ astro/src/test/java/com/emni/astro/AstroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emni.astro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
